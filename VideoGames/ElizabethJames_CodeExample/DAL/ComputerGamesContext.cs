00598381526a7686d07a9bd9bd261b2632fc65a8
ehjames04 <62249929+ehjames04@users.noreply.github.com>
2020-06-04 14:59:01 +0100

    add basic CRUD for online task

---
+++ VideoGames/ElizabethJames_CodeExample/DAL/ComputerGamesContext.cs
@@ -0,0 +1,21 @@
+﻿using ElizabethJames_CodeExample.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ElizabethJames_CodeExample.DAL
+{
+    public class ComputerGamesContext : DbContext
+    {
+
+        public ComputerGamesContext() : base("ComputerGamesContext")
+        {
+        }
+
+        public DbSet<ComputerGame> ComputerGames { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ VideoGames/ElizabethJames_CodeExample/DAL/ComputerGamesInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using ElizabethJames_CodeExample.Models;
+
+namespace ElizabethJames_CodeExample.DAL
+{
+    public class ComputerGamesInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<ComputerGamesContext>
+    {
+        protected override void Seed(ComputerGamesContext context)
+        {
+            var computerGames = new List<ComputerGame>
+            {
+                new ComputerGame{ Name="Minecraft",Description="Minecraft is all about building. You're dropped in a randomly generated world and can make tools and buildings out of raw materials.",ReleaseDate=DateTime.Parse("2011-11-18"),Rating=10}
+            };
+
+            computerGames.ForEach(cg => context.ComputerGames.Add(cg));
+            context.SaveChanges();
+        }
+    }
+}
